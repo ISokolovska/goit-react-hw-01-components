c821c37f5bdbbf7a79f0c372a9bbbc9b9f9dd42b
Iuliia Sokolovska <uu.sokil@gmail.com>
2022-11-16 22:34:04 +0100

    Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Profile } from "./Profile/Profile";
+import { user } from "user";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,17 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <h1>React homework template</h1>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        
+      />
     </div>
   );
 };
+
+
+
